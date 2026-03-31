ced7e2dfd5a2095a99a3cb701519e6353783ddb3
Eduardo Gonçalves <eduardo.ga@estudante.iftm.edu.br>
2026-03-31 20:30:54 -0300

    chore: setup inicial

---
+++ src/main/java/vexon/sellionpdv/SellionpdvApplication.java
@@ -0,0 +1,13 @@
+package vexon.sellionpdv;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SellionpdvApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SellionpdvApplication.class, args);
+	}
+
+}
---
+++ src/test/java/vexon/sellionpdv/SellionpdvApplicationTests.java
@@ -0,0 +1,13 @@
+package vexon.sellionpdv;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SellionpdvApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
